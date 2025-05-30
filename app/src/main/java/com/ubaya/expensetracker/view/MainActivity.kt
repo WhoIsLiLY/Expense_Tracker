ce71b0e80bdbb2f821dfd7ac30eda273fd2573a9
Foxrama123 <cahyanekresnanto@gmail.com>
2025-05-30 10:58:59 +0700

    UI Budget

--- app/src/main/java/com/ubaya/expensetracker/view/MainActivity.kt
+++ app/src/main/java/com/ubaya/expensetracker/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ubaya.expensetracker
+package com.ubaya.expensetracker.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ubaya.expensetracker.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

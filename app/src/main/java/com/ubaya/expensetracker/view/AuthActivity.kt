c62d34a55a02b0a982e6961907a8f06067d9c97a
Ryunite <lazyryu12@gmail.com>
2025-06-23 22:12:42 +0700

    Add Login Check

--- app/src/main/java/com/ubaya/expensetracker/view/AuthActivity.kt
+++ app/src/main/java/com/ubaya/expensetracker/view/AuthActivity.kt
@@ -1,5 +1,7 @@
 package com.ubaya.expensetracker.view
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,6 +12,17 @@ import com.ubaya.expensetracker.R
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val sharedPref = getSharedPreferences("ExpenseTrackerPrefs", Context.MODE_PRIVATE)
+
+        val isLoggedIn = sharedPref.getBoolean("IS_LOGGED_IN", false)
+
+        if (isLoggedIn) {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+            return
+        }
         setContentView(R.layout.activity_auth)
     }
 }
